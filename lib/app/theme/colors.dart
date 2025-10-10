066df3e1aaa23139aca234d9b7a9bdeec9d61a6f
FaroukBenHaj <mohamedfarouk.benhajamor@esprit.tn>
2025-10-10 17:48:21 +0100

    feat : added colors

---
+++ lib/app/theme/colors.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  //blue colors
+  static const Color blue_900 = Color(0xFF0C4180);
+  static const Color blue_700 = Color(0xFF1268CC);
+  static const Color blue_500 = Color(0xFF1782FF);
+  static const Color blue_300 = Color(0xFF74B4FF);
+  static const Color blue_100 = Color(0xFFD1E6FF);
+//LIght blue colors
+  static const Color light_blue_900 = Color(0xFF285B80);
+  static const Color light_blue_700 = Color(0xFF4091CC);
+  static const Color light_blue_500 = Color(0xFF50B5FF);
+  static const Color light_blue_300 = Color(0xFF96D3FF);
+  static const Color light_blue_100 = Color(0xFFDCF0FF);
+
+  //yellow colors
+  static const Color yellow_900 = Color(0xFFD1A300);
+  static const Color yellow_700 = Color(0xFFEBB700);
+  static const Color yellow_500 = Color(0xFFFFC700);
+  static const Color yellow_300 = Color(0xFFFFD84D);
+  static const Color yellow_100 = Color(0xFFFFEEB3);
+
+  //States
+  static const Color succes = Color(0xFF4CAF50);
+  static const Color warning = Color(0xFFFFC107);
+  static const Color error = Color(0xFFF44336);
+  static const Color info = Color(0xFF2196F3);
+
+  //bg
+  static const Color ligh_bg = Color(0xFFF7FAFC);
+  static const Color dark_bg = Color(0xFF2A3240);
+
+  //grey
+  static const Color grey_1000 = Color(0xFF101113);
+  static const Color grey_900 = Color(0xFF202326);
+  static const Color grey_800 = Color(0xFF40464d);
+  static const Color grey_700 = Color(0xFF687484);
+  static const Color grey_600 = Color(0xFF666687);
+  static const Color grey_500 = Color(0xFFA0AEC0);
+  static const Color grey_400 = Color(0xFFb3becd);
+  static const Color grey_300 = Color(0xFFc6ced9);
+  static const Color grey_200 = Color(0xFFd9dfe6);
+  static const Color grey_150 = Color(0xFFeceff2);
+  static const Color grey_100 = Color(0xFFF7FAFC);
+  static const Color grey_0 = Color(0xFFFFFFFF);
+
+}
