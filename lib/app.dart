979e55950fc6273a452e7f9dccb2dd115de7852b
FaroukBenHaj <mohamedfarouk.benhajamor@esprit.tn>
2025-11-14 00:11:13 +0100

    refactor : updated the structure

--- lib/app.dart
+++ lib/app.dart
@@ -2,19 +2,21 @@ import'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_app/features/authentication/views/onboarding.dart';
 import 'package:my_app/utils/theme/theme.dart';
-import 'package:my_app/pet/pages/home_page.dart';
-import 'package:my_app/pet/app/routes.dart'; // if you put appRoutes in a separate file
-class  App extends StatelessWidget {
-  const App ({super.key});
+import 'package:my_app/features/pet/pages/home_page.dart';
+import 'package:my_app/routes.dart'; // if you put appRoutes in a separate file
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  GetMaterialApp(
+    return GetMaterialApp(
+      title: 'PawLink',
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
       theme: CAppTheme.lightTheme,
       darkTheme: CAppTheme.darkTheme,
-      home: const HomePage(),
-      debugShowCheckedModeBanner: false,
+      initialRoute: '/onboarding', // optional
+      home: const HomePage(), // or FoodScreen if you want to start there
       routes: appRoutes,
     );
   }
---
+++ lib/routes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/features/pet/pages/home_page.dart';
+import 'package:my_app/features/pet/pages/pet_page.dart';
+import 'package:my_app/features/authentication/views/onboarding.dart';
+import 'package:my_app/features/food/views/food_screen.dart';
+
+final Map<String, WidgetBuilder> appRoutes = {
+//ONboarding screen
+  '/onboarding': (context) => const OnboardingPage1(),  
+//HOme 
+  '/home': (context) => const HomePage(),
+//Pet Page
+  '/pets': (context) => const PetPage(),
+//food
+  '/food': (context) => const FoodScreen(),
+};
